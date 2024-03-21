eedfbc74df1cf6451f242fc46d2f17269dfe5593
Bishwo Pratap Bhagat <contact.bishwo@gmail.com>
2024-03-21 11:42:01 +1000

    adding flowbite-react and config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
+import FlowbitePlugin from "flowbite/plugin";
 import type { Config } from "tailwindcss";
-
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
     extend: {
@@ -15,6 +16,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [FlowbitePlugin],
 };
 export default config;
