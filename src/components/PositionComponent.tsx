69002a3fcb38beae3395562d272724f0032b0e26
Bishwo Pratap Bhagat <contact.bishwo@gmail.com>
2024-03-21 11:46:21 +1000

    Changed div into a FlowBite Banner component

--- src/components/PositionComponent.tsx
+++ src/components/PositionComponent.tsx
@@ -3,7 +3,8 @@ import { InitialRobotPosition } from "@/constants"
 import useRobotStore from "@/store/RobotStore"
 import { Position } from "@/types/position"
 import { useEffect, useState } from "react"
-
+import { Banner } from 'flowbite-react';
+import { MdAnnouncement } from 'react-icons/md';
 /**
  * @return Returns the position of the robot
  */
@@ -16,11 +17,18 @@ export default function PositionComponent() {
   }, [positionState])
 
   return (
-    <div>
-      <p>Direction Information:
-        Top {'=>'} North, Bottom {'=>'} South, Right {'=>'} East, Left {'=>'} West
-      </p>
-      <p className="mb-4" data-testid="positionDesc">Position: ({robotPosition.x}, {robotPosition.y})</p>
-    </div>
+    <Banner>
+      <div className="flex  w-full border-b border-gray-200 bg-gray-50 p-4 dark:border-gray-600 dark:bg-gray-700">
+        <div className="mx-auto flex flex-col">
+          <p className="flex text-sm font-normal text-gray-500 dark:text-gray-400">
+            <MdAnnouncement className="mr-4 h-4 w-4" />
+            <span>
+              Direction Information: Top {'=>'} North, Bottom {'=>'} South, Right {'=>'} East, Left {'=>'} West
+            </span>
+          </p>
+          <p className="my-1 font-normal text-gray-500 dark:text-gray-400" data-testid="positionDesc">Position: ({robotPosition.x}, {robotPosition.y})</p>
+        </div>
+      </div>
+    </Banner>
   )
 }
