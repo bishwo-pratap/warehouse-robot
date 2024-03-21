cfe490a589ebfea8c8cfbb03096ba250be0638aa
Bishwo Pratap Bhagat <contact.bishwo@gmail.com>
2024-03-21 11:42:42 +1000

    removed className from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-sky-300">{children}</body>
+      <body>{children}</body>
     </html >
   );
 }
