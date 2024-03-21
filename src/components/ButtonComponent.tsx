d3f79fcacadb6580bc1adb8380b615001a3f8afa
Bishwo Pratap Bhagat <contact.bishwo@gmail.com>
2024-03-21 11:44:48 +1000

    Moved Button div into a FlowBite Sidebar component

--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
+import { Sidebar } from 'flowbite-react';
 import useRobotStore from "@/store/RobotStore"
 
 export default function ButtonComponent() {
   const handleActions = useRobotStore((state) => state.handleActions)
   const btnClass = 'rounded-full flex items-center justify-center space-x-2 py-2 px-4 rounded mr-2 bg-gray-400 text-white hover:bg-gray-500 m-2';
   return (
-    <div className="my-4">
-      <div className=" mr-4">
-        <div className="flex flex-col items-center justify-between ">
-          <h3 className="text-xl font-bold mb-1">Movements:</h3>
-          <button data-testid="moveUp" className={btnClass} onClick={() => handleActions('N')}>Move Up</button>
-          <div className="flex ">
-            <button data-testid="moveLeft" className={btnClass} onClick={() => handleActions('W')}>Move Left</button>
-            <button data-testid="moveRight" className={btnClass} onClick={() => handleActions('E')}>Move Right</button>
+    <Sidebar aria-label="actions-sidebar" className='h-100 w-30'>
+      <div className="my-4">
+        <div className="mr-4">
+          <div className="flex flex-col items-center justify-between ">
+            <h3 className="text-xl font-bold mb-1">Movements:</h3>
+            <button data-testid="moveUp" className={btnClass} onClick={() => handleActions('N')}>Move Up</button>
+            <div className="flex ">
+              <button data-testid="moveLeft" className={btnClass} onClick={() => handleActions('W')}>Move Left</button>
+              <button data-testid="moveRight" className={btnClass} onClick={() => handleActions('E')}>Move Right</button>
+            </div>
+            <button data-testid="moveDown" className={btnClass} onClick={() => handleActions('S')}>Move Down</button>
           </div>
-          <button data-testid="moveDown" className={btnClass} onClick={() => handleActions('S')}>Move Down</button>
         </div>
       </div>
-    </div>
+    </Sidebar>
   )
 }
